0d8651d7d39cca50847a448a270d60c096430b5f
shewaeger <shewaeger@gmail.com>
2019-09-29 09:11:58 +0300

    task 3

--- Laba1/Program.cs
+++ Laba1/Program.cs
@@ -6,7 +6,7 @@ namespace Laba1
     {
         static void N1()
         {
-            Console.Write("Введите число:");
+            Console.Write("Введите число: ");
             string xString = Console.ReadLine().Replace('.', ','); // Считываем число и заменяем точку на запятую.
             double x = Convert.ToDouble(xString); // Преобразуем число из String в Double
             x *= 10; // Заносим в целую часть первую цифру после запятой
@@ -25,7 +25,57 @@ namespace Laba1
 
         static void N2()
         {
+            Console.Write("Введите секунды: ");
+            string secondsS = Console.ReadLine();
+            ulong totalSeconds = Convert.ToUInt64(secondsS);
+            ulong hours = totalSeconds / 3600;
+            totalSeconds -= (hours) * 3600;
+            ulong minutes = totalSeconds / 60;
+            totalSeconds -= (minutes) * 60;
+
+            Console.WriteLine(String.Format("h: {0}; m: {1}; s: {2}", hours, minutes, totalSeconds));
+            // Подставляем значения в нужные места строки (обозначены как {число}) 
+        }
+
+        static void N3()
+        {
+            Console.Write("Введите h (0 <= h <= 11): ");
+            string hS = Console.ReadLine();
             
+            int h = Convert.ToInt32(hS);            
+            if(h < 0 || h > 11)
+            {
+                Console.WriteLine("Неверно введено h");
+                return;
+            }
+            
+            Console.Write("Введите m (0 <= m <= 59): ");
+            string mS = Console.ReadLine();
+            
+            int m = Convert.ToInt32(mS);
+            if (m < 0 || m > 59)
+            {
+                Console.Write("Неверно введено m");
+                return;
+            }
+
+            Console.WriteLine("Введите s (0 <= s <= 59): ");
+            string sS = Console.ReadLine();
+
+            int s = Convert.ToInt32(sS);
+            if (s < 0 || s > 59)
+            {
+                Console.WriteLine("Неверно введено s");
+                return;
+            }
+
+            double hAngle = 360.0 / 12.0;
+            double mAngle = hAngle / 60.0;
+            double sAngle = mAngle / 60.0;
+
+            double result = hAngle * h + mAngle * m + sAngle * s;
+
+            Console.WriteLine(String.Format("Угол поворота стрелки: {0}", result));
         }
         static void Main(string[] args)
         {
@@ -33,6 +83,8 @@ namespace Laba1
             N1();
             Console.WriteLine("Задание 2");
             N2();
+            Console.WriteLine("Задание 3");
+            N3();
         }
     }
 }
